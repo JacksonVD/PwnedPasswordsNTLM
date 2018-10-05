cb6ee45233d8234637e65c3cef24ae9dcb56a0bd
Jackson Van Dyke <jacksonvd@live.com.au>
2018-10-06 00:07:05 +0800

    Added support for frequency in lines
    Tidied up, added more detailed comments, added support for sorted files with password frequencies. Should hopefully work fine!

--- PwnedPasswordsNTLM.cs
+++ PwnedPasswordsNTLM.cs
@@ -1,72 +1,84 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 // Basic binary search to be used on a sorted Pwned Passwords dataset
 
 class PwnedPasswordsNTLM
 {
-
+    /*
+     * METHOD: main
+     * PARAMS: args (String[])
+     *           Three arguments - data set location, hashes location, output location
+     * PURPOSE: Flow of execution - opens files, calls file reader function, closes files
+     * EXPORTS: NONE
+     */
     static void Main(String[] args)
     {
-        if (args.Length!=0)
+        // Confirm correct number of arguments given - dataset, input, output locations
+        if (args.Length == 3)
         {
-            if (args[0] != null && args[1] != null && args[2] != null)
+            try
             {
-                // Input and output files
-                try
+                // Stopwatch for timing
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+                // Attempt to open the input and output files
+                System.IO.StreamReader inFile = new System.IO.StreamReader(@args[1]);
+                System.IO.StreamWriter outFile = new System.IO.StreamWriter(@args[2]);
+
+                String line;
+
+                // Continue to read the input file while not EOF
+                while ((line = inFile.ReadLine()) != "" && line != null)
                 {
-                    // Stopwatch for timing
-                    Stopwatch stopwatch = new Stopwatch();
-                    stopwatch.Start();
-
-                    System.IO.StreamReader inFile = new System.IO.StreamReader(@args[1]);
-                    System.IO.StreamWriter outFile = new System.IO.StreamWriter(@args[2]);
-
-                    String line;
-
-                    // Continue to read the input file while not EOF
-                    while ((line = inFile.ReadLine()) != "" && line != null)
+                    // If readFile returns true, password exists in the dataset
+                    if (readFile(line.Split(':')[1], args[0]))
                     {
-                        // If readFile returns true, password exists in the dataset
-                        if (readFile(line.Split(':')[1], args[0]))
-                        {
-                            // Output to the output file that the user has a breached password
-                            outFile.WriteLine("{0} has a pwned password", line.Split(':')[0]);
-                            Console.WriteLine("{0} has a pwned password", line.Split(':')[0]);
-                        }
-
+                        // Output to the output file that the user has a breached password
+                        outFile.WriteLine("{0} has a pwned password", line.Split(':')[0]);
+                        Console.WriteLine("{0} has a pwned password", line.Split(':')[0]);
                     }
+                }
 
-                    // Stop stopwatch and output time taken
-                    stopwatch.Stop();
-                    Console.WriteLine("\nCompleted in: {0} ms", stopwatch.ElapsedMilliseconds);
-                    Console.WriteLine("All pwned users output to {0}", @args[2]);
+                // Stop stopwatch and output time taken
+                stopwatch.Stop();
+                Console.WriteLine("\nCompleted in: {0} ms", stopwatch.ElapsedMilliseconds);
+                Console.WriteLine("All pwned users output to {0}", @args[2]);
 
-                    // Close the output and input files after reading and writing
-                    outFile.Close();
-                    inFile.Close();
-                }
-                catch (FileNotFoundException)
-                {
-                    Console.WriteLine("File does not exist\n");
-                }
-                catch (IOException)
-                {
-                    Console.WriteLine("File I/O error\n");
-                }
+                // Close the output and input files after reading and writing
+                outFile.Close();
+                inFile.Close();
+            }
+
+            // Output to user if file not found or I/O errors occur
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File does not exist\n");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("File I/O error\n");
             }
         }
+        // If the user doesn't supply the correct parameters, output the program's usage
         else
         {
             Console.WriteLine("Incorrect parameters.\nUsage: PwnedPasswordsNTLM <DATASET> <INPUT FILE> <OUTPUT FILE>");
         }
     }
 
+    /*
+     * METHOD: readFile
+     * PARAMS: searchValue (String)
+     *           Value to search for - the NTLM hash
+     *         inFile (String)
+     *           Name of the input file
+     * PURPOSE: Binary search method to look for the current hash in the input file
+     * EXPORTS: passwordFound (Boolean)
+     *            Whether or not the hash exists in the input file
+     */
     static Boolean readFile(String searchValue, String inFile)
     {
         // Declaration and initialisation for variables to be used
@@ -129,6 +141,7 @@ class PwnedPasswordsNTLM
             // Close the file after reading
             file.Close();
         }
+        // Throw IOExceptions up to main
         catch
         {
             throw new IOException();
@@ -138,6 +151,16 @@ class PwnedPasswordsNTLM
         return passwordFound;
     }
 
+    /*
+     * METHOD: getRecord
+     * PARAMS: inFile (FileStream)
+     *           File stream for the input file
+     *         position (Int64)
+     *           Current position in the file to grab the next hash from
+     * PURPOSE: Gets the next hash from the file stream given the current position
+     * EXPORTS: record (String)
+     *            The hash obtained from the required position
+     */
 
     static String getRecord(FileStream inFile, Int64 position)
     {
@@ -150,12 +173,37 @@ class PwnedPasswordsNTLM
 
         // Read 32 chararacters (size of 1 NTLM hash) into the buffer byte array
         inFile.Read(buffer, 0, 32);
-
+           
         // Convert from byte array to string and remove null terminator character
         record = System.Text.Encoding.UTF8.GetString(buffer);
+
+        // In order to allow for users using the frequency of occurrences, this check
+        // is required - essentially scan for a \n in the current string. If it exists,
+        // it means that the entire hash in the current line has not been picked up - it has
+        // part of the hash before due to the frequency messing with the number of characters
+        if (record.Contains("\n"))
+        {
+            // Clear the buffer
+            Array.Clear(buffer, 0, buffer.Length);
+
+            // Anything before the \n in the string is a different hash
+            String oldHash = record.Split('\n')[0];
+
+            // Get the hash length including the \n
+            int oldHashLength = oldHash.Length+1;
+
+            // Read oldHashLength more characters into our buffer
+            inFile.Read(buffer, 0, oldHashLength);
+
+            // Get the part of the current hash that we already have, and append the rest of the hash
+            record = record.Split('\n')[1];
+            record = record + System.Text.Encoding.UTF8.GetString(buffer);
+        }
+
+        // Replace the NULL character with string.Empty
         record = record.Replace("\0", string.Empty);
 
-        // Return buffer as a string
+        // Return the current hash
         return record;
     }
 }
